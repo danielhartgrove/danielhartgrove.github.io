b20bc0a48b2ae7b0fe07468cd12e59953371e29c
Dan Hartgrove <hartgrovedaniel3@outlook.com>
2023-08-03 17:48:41 +0100

    add(feat): add autoclicking to coding clicker

--- game/game.js
+++ game/game.js
@@ -1,12 +1,20 @@
 //initial number of cookies    
 var num = 0;
-var cpc = 1;
+var cpc = 1;    //cookies per click
+var lps = 0;    //lines per second
 
 window.onload = function () {
   var numbers = document.getElementById("numbers");
   
   numbers.innerHTML = 0;
-};
+  linesPerSecond.innerHTML = 0;
+
+  setInterval(function() {
+        num += cpc*(lps/5);
+        numbers.innerHTML = num.toFixed(0);
+},200);
+
+}
 
 var cookie = document.getElementById("cookie");
 
@@ -19,13 +27,30 @@ function cookieClick() {
     var upgradeLevel = document.getElementById("upgradeLevel");
     
     numbers.innerHTML = num.toFixed(0);  
+   
 }
+
+
 
 function buyItem(cost, amount){
     if(num >= cost){
         num -= cost;
         cpc *= amount;
 
+        var numbers = document.getElementById("numbers");
+        numbers.innerHTML = num.toFixed(0);
+    }else{
+        alert ("You haven't written enough lines!");
+    }
+}
+
+function buyAuto(cost, amount){
+    if(num >= cost){
+        num -= cost;
+        lps += amount;
+
+        var linesPerSecond = document.getElementById("linesPerSecond");
+        linesPerSecond.innerHTML = lps;
         var numbers = document.getElementById("numbers");
         numbers.innerHTML = num.toFixed(0);
     }else{
