d2d9c5a90a0a3e135b551ee577fd76fcdd3aa591
Daniel Hartgrove <hartgrovedaniel3@outlook.com>
2025-01-17 02:05:54 +0000

    fix drum machine

--- projects/drum_machine/script.js
+++ projects/drum_machine/script.js
@@ -1,8 +1,8 @@
 function playAudio(audioId) {
   const audio = document.getElementById(audioId);
   if (audio) {
-    console.log(audio);
     audio.play();
+   
   } else {
     console.error("ERROR: AUDIO NOT FOUND");
   }
@@ -14,7 +14,6 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     const audioId = button.innerText;
-    console.log(audioId);
     playAudio(audioId);
   });
 });
@@ -22,6 +21,5 @@ buttons.forEach(button => {
 document.addEventListener('keydown', (event) => {
   const key = event.key.toUpperCase(); 
   const audioId = key;
-  console.log(audioId);
   playAudio(audioId);
 });
